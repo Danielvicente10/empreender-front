7bffc234c720703f73be7024a02cf279e2d89121
Daniel Vicente <danielvicente63@gmail.com>
2023-09-19 21:50:52 -0300

    Projeto React.js pt1

---
+++ src/components/MainHeader/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+     grid-area: MH;
+     background-color: ${props => props.theme.colors.secundary};
+     color: ${props => props.theme.colors.white};
+`;
+
---
+++ src/pages/Dashboard/Index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return(
+        <h1>Mauricio Gay</h1>
+    );
+}
+
+export default Dashboard;
