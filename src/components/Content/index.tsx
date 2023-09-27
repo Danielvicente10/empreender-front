6e916d4411f356a5012cde6934b666054fc49889
Daniel Vicente <danielvicente63@gmail.com>
2023-09-26 22:28:10 -0300

    Colocando scrollbar

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,16 +3,13 @@ import { Container } from './styles';
 
 interface ContentProps {
     children: ReactNode;
-  }
-  
+}  
 
 const Content: React.FC<ContentProps>= ({ children }) => {
     return(
-
         <Container>
             {children}
-        </Container>     
-        
+        </Container>        
     );
 }
 
--- src/pages/List/Index.tsx
+++ src/pages/List/Index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container } from './syles';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectIpunt';
+import Content from '../../components/Content';
+import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
 const List: React.FC = () => {
 
@@ -16,6 +18,99 @@ const List: React.FC = () => {
            <ContentHeader title='List' lineColor='#FFF'>
                 <SelectInput options={options}/>
             </ContentHeader>
+
+            <Content>
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+                
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+
+                <HistoryFinanceCard 
+                title='Conta de Luz'
+                subtitle='26/09/2023'
+                cardColor='#313862'
+                tagColor='#e44c4e'
+                amount='R$ 130,00'
+                
+                />
+                
+            </Content>
         </Container>
         
     );
